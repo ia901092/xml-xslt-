a2de3ac0f628d45bf190ee254b3297c329f17c42
ia901092 <ia901092@gmail.com>
2026-05-02 16:57:37 +0200

    Skapa formulär med dropdown från webservice

---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form</title>
+</head>
+<body>
+
+<form action="responseXSLT.php" method="get">
+    <select name="paper">
+<?php
+$xml = simplexml_load_file("https://wwwlab.webug.se/examples/XML/articleservice/papers/");
+
+foreach ($xml->NEWSPAPER as $paper) {
+    echo "        <option value=\"" . $paper['TYPE'] . "\">" . $paper['NAME'] . "</option>\n";
+}
+?>
+    </select>
+    <input type="submit" value="Visa">
+</form>
+
+</body>
+</html>
